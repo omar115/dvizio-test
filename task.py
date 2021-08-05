c694557c2940c901c62a89dbf7dc5f26379732dc
omar115 <omarhasan115@gmail.com>
2021-08-05 20:58:02 +0600

    project setup done

--- task.py
+++ task.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import traceback
+import re
 import time
-
 
 def init_driver():
     driver = webdriver.Chrome()
@@ -16,7 +16,7 @@ def login(driver):
     
     goto_link = 'https://www.tred.com/buy?body_style=&distance=50&exterior_color_id=&make=&miles_max=100000&miles_min=0&model=&page_size=24&price_max=100000&price_min=0&query=&requestingPage=buy&sort=desc&sort_field=updated&status=active&year_end=2022&year_start=1998&zip='
     try:
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(20)
         driver.get(goto_link)
     except:
         print(traceback.format_exc())
@@ -36,4 +36,33 @@ def scrape_data(driver, radius, zipcode):
     xpath_link = '/html/body/section/div/div/div[3]/div/section/div/div[1]/div/section/form/div[1]/div[2]/div[2]/input'
 
     driver.find_element_by_xpath(xpath_link).send_keys(zipcode)
+
+    time.sleep(3)
+
+    xpath_text = '/html/body/section/div/div/div[3]/div/section/div/div[2]/div[1]/div/div[1]/div/div/div/a/div[2]/h5'
+
+    li = []
+    i = 1
+    while i > 0:
+        print('---i is---',i)
+        xpath_text = '/html/body/section/div/div/div[3]/div/section/div/div[2]/div[1]/div/div['+str(i)+']/div/div/div/a/div[2]/h5'
+        # try:
+        name = driver.find_element_by_xpath(xpath_text)
+        
+        if name.text == '' or name.text == 'nan':
+            i = -1
+        else:
+            driver.implicitly_wait(10)
+            li.append(name.text)
+            # print(li)
+            # driver.find_element_by_name(li[0]).click()
+            # driver.find_element_by_xpath(xpath_text).click()
+            # xpath_text2 = '/html/body/section/div/div/div[2]/div[5]/div[2]/div/h1[1]'
+            # name = driver.find_element_by_xpath(xpath_text2)
+            # name = name.text
+            # print('Car Name is: ', name)
+            # i = i + 1
+        i = i + 1
+    print(li)
+
 

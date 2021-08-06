f7c4b5abfc04261845b429fe232be30577c7f386
omar115 <omarhasan115@gmail.com>
2021-08-06 10:22:49 +0600

    complete exam

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from os import name
 import task
 import traceback
+import pandas as pd
+import numpy as np
 
+# driver code
 def main():
-
+    
     radius, zipcode = task.user_input()
 
     driver = task.init_driver()
     
+    print("---Process Started---")
+
     task.login(driver)
+    
+    df = task.scrape_data(driver, radius, zipcode)
 
-    task.scrape_data(driver, radius, zipcode)
+    # remove all whitespace
+    df.replace(r'^\s*$', np.nan, regex=True)
 
+    #remove null
+    df = df.apply(lambda x: pd.Series(x.dropna().values))
+
+    #save CSV
+    df.to_csv(r'C:\Users\Ferntech\final_output.csv', index=False, header=True, mode='a')
+
+    driver.quit()
+
+    print("---Process Finished---")
 
 main()
--- task.py
+++ task.py
@@ -5,30 +5,39 @@ import pandas as pd
 import traceback
 import re
 import time
+import numpy as np
 
+# function to initialize the chromedriver
 def init_driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path=r'C:\Users\Ferntech\Desktop\chromedriver.exe')
     driver.maximize_window()
     driver.delete_all_cookies()
     return driver
 
+# login the portal
 def login(driver):
     
     goto_link = 'https://www.tred.com/buy?body_style=&distance=50&exterior_color_id=&make=&miles_max=100000&miles_min=0&model=&page_size=24&price_max=100000&price_min=0&query=&requestingPage=buy&sort=desc&sort_field=updated&status=active&year_end=2022&year_start=1998&zip='
     try:
         driver.implicitly_wait(20)
         driver.get(goto_link)
+        
     except:
         print(traceback.format_exc())
 
 # take input of radius and zipcode from the user
 def user_input():
     
-    radius = input("Write your Radius: ")
-    zipcode = input("Write your zipcode: ")
+    radius = input("Write your Radius including mi, i.e. 50 mi.: ")
+    zipcode = input("Write your zipcode in numbers i.e. 1000: ")
     return radius, zipcode
 
+# scrape data from the portal and save it to a CSV
 def scrape_data(driver, radius, zipcode):
+    li = []     # empty list
+    df = pd.DataFrame()     # initialize dataframe
+
+    # select radius, input zipcode
     select = Select(driver.find_element_by_xpath('/html/body/section/div/div/div[3]/div/section/div/div[1]/div/section/form/div[1]/div[2]/div[1]/select'))
 
     select.select_by_visible_text(radius)
@@ -41,28 +50,62 @@ def scrape_data(driver, radius, zipcode):
 
     xpath_text = '/html/body/section/div/div/div[3]/div/section/div/div[2]/div[1]/div/div[1]/div/div/div/a/div[2]/h5'
 
-    li = []
+    # loop to scrape all cards and scrape data
     i = 1
     while i > 0:
-        print('---i is---',i)
+
         xpath_text = '/html/body/section/div/div/div[3]/div/section/div/div[2]/div[1]/div/div['+str(i)+']/div/div/div/a/div[2]/h5'
-        # try:
-        name = driver.find_element_by_xpath(xpath_text)
         
-        if name.text == '' or name.text == 'nan':
-            i = -1
+        try:
+            name = driver.find_element_by_xpath(xpath_text)
+        except:
+            return df
+        try:
+            if name.text == '' or name.text == 'nan':
+                i = -2
+        except:
+            pass
+
         else:
-            driver.implicitly_wait(10)
-            li.append(name.text)
-            # print(li)
-            # driver.find_element_by_name(li[0]).click()
-            # driver.find_element_by_xpath(xpath_text).click()
-            # xpath_text2 = '/html/body/section/div/div/div[2]/div[5]/div[2]/div/h1[1]'
-            # name = driver.find_element_by_xpath(xpath_text2)
-            # name = name.text
-            # print('Car Name is: ', name)
-            # i = i + 1
+            car_name=''
+            car_price=''
+            data=''
+            try:
+                driver.implicitly_wait(10)
+                
+                driver.find_element_by_xpath(xpath_text).click()
+                xpath_text2 = '/html/body/section/div/div/div[2]/div[5]/div[2]/div/h1[1]'
+                
+                name = driver.find_element_by_xpath(xpath_text2)
+                web_name = name.text
+
+                # using re extract the year from text, and slice the string to get required value
+                date = re.findall(r'.*([1-3][0-9]{3})',web_name)
+                start = web_name.find(date[0])
+                car_name = web_name[start:-9]
+                print('Car name is : ', car_name)
+
+                # extract price name
+                price_xpath = '/html/body/section/div/div/div[2]/div[4]/div/div/div[2]/div/div/h2'
+                try:
+                    car_price = driver.find_element_by_xpath(price_xpath)
+                    car_price = car_price.text
+                    print('Car price is ', car_price)
+                except:
+                    car_price = 'SOLD'
+
+                # extract table data
+                for table in driver.find_elements_by_xpath('//*[contains(@id,"summary-table")]//tr'):
+                    data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
+                    
+            except:
+                print(traceback.format_exc())
+                return df
+            
+            # append data to dataframe
+            df = df.append({'Name': car_name,'Price':car_price,'Vehicle Summary':data}, ignore_index=True)
+
+            driver.back()
+
         i = i + 1
-    print(li)
-
-
+    
